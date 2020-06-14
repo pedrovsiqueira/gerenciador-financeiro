ce9659105beebc0e7d51c2574f1f9f46945dd4f9
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-14 16:10:44 -0300

    add knexfile

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+module.exports = {
+  test: {
+    client: "pg",
+    version: "9.6",
+    connection: {
+      host: "Gerenciador_Financeiro",
+      user: "postgres",
+      password: "P3dr0",
+      database: "gerenciador_financeiro",
+    },
+  },
+};
