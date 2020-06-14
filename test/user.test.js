55d8d45d6908d65ff86732ced191c51ab8713344
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-14 14:33:41 -0300

    Add test for inserting user

--- test/user.test.js
+++ test/user.test.js
@@ -8,3 +8,11 @@ test("Deve listar todos os usuários", async () => {
   expect(resp.body).toHaveLength(1);
   expect(resp.body[0]).toHaveProperty("name", "John Doe");
 });
+
+test("Deve inserir usuário com sucesso", async () => {
+  const resp = await request(app)
+    .post("/users")
+    .send({ name: "Pedro Siqueira", email: "pedrosiqueira@gmail.com" });
+  expect(resp.status).toBe(201);
+  expect(resp.body.name).toBe("Pedro Siqueira");
+});
