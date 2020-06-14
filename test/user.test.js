afa1cb73a91d8b13078bd6438fc60c789d3f389f
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-14 14:15:38 -0300

    finish user.test.js

--- test/user.test.js
+++ test/user.test.js
@@ -2,7 +2,12 @@ const request = require("supertest");
 
 const app = require("../src/app");
 
-test("Deve listar todos os usuários", async () => {
-  const resp = await request(app).get("/users");
-  return expect(resp.status).toBe(200);
+test("Deve listar todos os usuários", () => {
+  return request(app)
+    .get("/users")
+    .then((resp) => {
+      expect(resp.status).toBe(200);
+      expect(res.body).toHaveLength(1);
+      expect(res.body[0]).toHaveProperty("name", "John Doe");
+    });
 });
