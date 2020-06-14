267c94389bd8f2b35ac4f7b29943720c3f8f9ef1
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-14 14:11:02 -0300

    update folder structure and requires

--- src/test/user.test.js
+++
@@ -1,5 +0,0 @@
-const request = require("supertest");
-
-test('', () => {
-  
-})
---
+++ test/user.test.js
@@ -0,0 +1,7 @@
+const request = require("supertest");
+
+const app = require('../src/app')
+
+test('Deve listar todos os usuários', () => {
+  
+})
