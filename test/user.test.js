befebea5265fce38a8f872a56556ca8cbe3d173d
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-14 14:13:14 -0300

    add user test to check if its listing all users

--- test/user.test.js
+++ test/user.test.js
@@ -1,7 +1,8 @@
 const request = require("supertest");
 
-const app = require('../src/app')
+const app = require("../src/app");
 
-test('Deve listar todos os usuários', () => {
-  
-})
+test("Deve listar todos os usuários", async () => {
+  const resp = await request(app).get("/users");
+  return expect(resp.status).toBe(200);
+});
