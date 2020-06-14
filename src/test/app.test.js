45738a06829ce03d1db95167d1aec774fa6c784e
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-14 13:51:03 -0300

    update app.test

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -3,6 +3,6 @@ const request = require("supertest");
 const app = require("../src/app");
 
 test("Deve responder na raiz", async () => {
-  const resp = request(app.get("/"));
+  const resp = await request(app).get("/");
   return expect(resp.status).toBe(200);
 });
