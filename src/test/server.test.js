eac5cdec5181fa69388a7091b34429aba3892755
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-14 13:32:06 -0300

    add test to see if server responds on port 3001

--- src/test/server.test.js
+++ src/test/server.test.js
@@ -1,6 +1,11 @@
-test("Deve responder na porta 3001", () => {
-  //acessar a url http://localhost:3001
+const supertest = require("supertest");
 
+//como não temos a rota ainda, utilizamos qualquer link para fazer o teste
+const request = supertest("http://localhost:3001");
+
+test("Deve responder na porta 3001", async () => {
+  //acessar a url http://localhost:3001
   //verificar que a resposta foi 200
-  
+  const resp = await request.get("/");
+  return expect(resp.status).toBe(200);
 });
